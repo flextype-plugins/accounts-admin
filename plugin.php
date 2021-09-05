e11890e290b4b59c792f9ffd13a4ec193fa985fd
Awilum <sergey.romanenko@flextype.org>
2021-09-05 21:06:17 +0300

    feat(core): basic code base updates for accounts admin

--- plugin.php
+++ plugin.php
@@ -2,6 +2,41 @@
 
 declare(strict_types=1);
 
-namespace Flextype;
+/**
+ * Flextype Admin Plugin
+ *
+ * @link https://flextype.org
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
-include __DIR__ . '/bootstrap.php';
+namespace Flextype\Plugin\AccountsAdmin;
+
+use function is_file;
+
+/**
+ * Set base admin route
+ */
+$adminRoute = registry()->get('plugins.admin.settings.route');
+
+/**
+ * Ensure vendor libraries exist
+ */
+! is_file($accountsAdminAutoload = __DIR__ . '/vendor/autoload.php') and exit('Please run: <i>composer install</i> accounts admin plugin');
+
+/**
+ * Register The Auto Loader
+ *
+ * Composer provides a convenient, automatically generated class loader for
+ * our application. We just need to utilize it! We'll simply require it
+ * into the script here so that we don't have to worry about manual
+ * loading any of our classes later on. It feels nice to relax.
+ * Register The Auto Loader
+ */
+$accountsAdminLoader = require_once $accountsAdminAutoload;
+
+/**
+ * Include web routes
+ */
+require_once 'src/accounts-admin/routes/web.php';
